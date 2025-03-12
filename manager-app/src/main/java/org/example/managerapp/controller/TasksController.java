88f70ddbda7272dd9d34a43b729471ed53ab84ed
newrav1k <kritsky.academi@gmail.com>
2025-03-12 12:36:21 +0300

    Added TasksController

---
+++ manager-app/src/main/java/org/example/managerapp/controller/TasksController.java
@@ -0,0 +1,37 @@
+package org.example.managerapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.managerapp.entity.Task;
+import org.example.managerapp.service.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
+public class TasksController {
+
+    private final TaskService taskService;
+
+    @GetMapping
+    public String tasks(Model model) {
+        model.addAttribute("tasks", this.taskService.findAll());
+        return "/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreateTask(@ModelAttribute Task task, Model model) {
+        model.addAttribute("task", task);
+        return "/tasks/new-task";
+    }
+
+    @PostMapping("/create")
+    public String createTask(@ModelAttribute Task task) {
+        return "/index";
+    }
+
+}
