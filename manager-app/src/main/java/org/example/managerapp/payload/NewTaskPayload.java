a9f41b420b6978c5fbeefe91d136200b355993a7
newrav1k <kritsky.academi@gmail.com>
2025-03-14 01:49:39 +0300

    Added validation to NewTaskPayload

--- manager-app/src/main/java/org/example/managerapp/payload/NewTaskPayload.java
+++ manager-app/src/main/java/org/example/managerapp/payload/NewTaskPayload.java
@@ -1,13 +1,18 @@
 package org.example.managerapp.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.example.managerapp.entity.TaskStatus;
 
 import java.time.LocalDateTime;
 
 public record NewTaskPayload(
-        String title,
-        String description,
-        TaskStatus status,
-        LocalDateTime deadline) {
+        @NotNull
+        @Size(min = 3, max = 50, message = "{tasks.title.error.size}") String title,
+        @NotBlank(message = "{tasks.description.error.null}")
+        @Size(max = 500, message = "{tasks.description.error.size}") String description,
+        @NotNull TaskStatus status,
+        @NotNull LocalDateTime deadline) {
 
 }
