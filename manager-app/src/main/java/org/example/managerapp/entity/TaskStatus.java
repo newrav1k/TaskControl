15120bcb667b4e68c1c02e5b062928d92283798e
newrav1k <kritsky.academi@gmail.com>
2025-03-17 17:19:58 +0300

    Fixed TaskStatus

--- manager-app/src/main/java/org/example/managerapp/entity/TaskStatus.java
+++ manager-app/src/main/java/org/example/managerapp/entity/TaskStatus.java
@@ -1,5 +1,17 @@
 package org.example.managerapp.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum TaskStatus {
-    NEW, IN_PROGRESS, COMPLETED
+    NEW("Новая"),
+    IN_PROGRESS("В процессе"),
+    COMPLETED("Завершена");
+
+    private final String name;
+
+    TaskStatus(String name) {
+        this.name = name;
+    }
+
 }
