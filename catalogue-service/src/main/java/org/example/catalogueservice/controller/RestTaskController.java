9083aa80d995f9fd3e247c63a89c2c9ceade6b08
newrav1k <kritsky.academi@gmail.com>
2025-03-19 00:14:50 +0300

    Added @ExceptionHandler(NoSuchElementException.class) and BindException handler to RestTaskController

--- catalogue-service/src/main/java/org/example/catalogueservice/controller/RestTaskController.java
+++ catalogue-service/src/main/java/org/example/catalogueservice/controller/RestTaskController.java
@@ -1,11 +1,18 @@
 package org.example.catalogueservice.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.catalogueservice.entity.Task;
 import org.example.catalogueservice.payload.NewTaskPayload;
 import org.example.catalogueservice.service.TaskService;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -23,10 +31,12 @@ public class RestTaskController {
 
     private final TaskService taskService;
 
+    private final MessageSource messageSource;
+
     @ModelAttribute("task")
     public Task task(@PathVariable Integer taskId) {
         return this.taskService.findTaskById(taskId)
-                .orElseThrow(() -> new NoSuchElementException("tasks.not.found"));
+                .orElseThrow(() -> new NoSuchElementException("task.not.found"));
     }
 
     @GetMapping
@@ -36,16 +46,32 @@ public class RestTaskController {
 
     @PostMapping("/update")
     public ResponseEntity<Void> updateTask(@PathVariable("taskId") Integer taskId,
-                                           @RequestBody NewTaskPayload payload) {
-        this.taskService.updateTask(taskId, payload.title(),
-                payload.description(), payload.status(), payload.deadline());
-        return ResponseEntity.noContent().build();
+                                           @Valid @RequestBody NewTaskPayload payload,
+                                           BindingResult bindingResult) throws BindException {
+        if (bindingResult.hasErrors()) {
+            if (bindingResult instanceof BindException exception) {
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
+        } else {
+            this.taskService.updateTask(taskId, payload.title(),
+                    payload.description(), payload.status(), payload.deadline());
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<Void> deleteTask(@PathVariable("taskId") Integer taskId) {
         this.taskService.deleteTaskById(taskId);
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ProblemDetail> handleNoSuchElementException(NoSuchElementException exception, Locale locale) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                this.messageSource.getMessage(exception.getMessage(), new Object[0], exception.getMessage(), locale));
+        return ResponseEntity.of(problemDetail).build();
     }
 
 }
