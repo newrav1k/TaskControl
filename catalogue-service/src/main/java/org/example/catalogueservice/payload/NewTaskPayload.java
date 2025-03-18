89855b17a3ddd8970f7c6672fa77e20ad4e54a48
newrav1k <kritsky.academi@gmail.com>
2025-03-18 19:50:44 +0300

    Added validation to NewTaskPayload and default messages to messages.properties

--- catalogue-service/src/main/java/org/example/catalogueservice/payload/NewTaskPayload.java
+++ catalogue-service/src/main/java/org/example/catalogueservice/payload/NewTaskPayload.java
@@ -1,10 +1,18 @@
 package org.example.catalogueservice.payload;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.example.catalogueservice.entity.TaskStatus;
 
 import java.time.LocalDateTime;
 
-public record NewTaskPayload(String title, String description,
-                             TaskStatus status, LocalDateTime deadline) {
+public record NewTaskPayload(
+        @NotBlank(message = "{payload.title.error.blank}") @Size(max = 50, message = "{payload.title.error.size}") String title,
+        @NotBlank(message = "{payload.description.error.blank}") @Size(max = 250, message = "{payload.description.error.size}") String description,
+        @NotNull(message = "{payload.status.error.null}") TaskStatus status,
+        @NotNull(message = "{payload.deadline.error.null}") @FutureOrPresent(message = "{payload.deadline.error.future}") LocalDateTime deadline
+) {
 
 }
