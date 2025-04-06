49e96a3b39b3221568434ec157b53e02517489a6
newrav1k <kritsky.academi@gmail.com>
2025-04-06 14:16:48 +0300

    Added security dependency to catalogue-service

---
+++ catalogue-service/src/main/java/org/example/catalogueservice/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package org.example.catalogueservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().hasAuthority("ROLE_SERVICE"))
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
