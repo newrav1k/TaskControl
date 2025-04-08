312904e92d7ee3b7f9db168b8d0d6873717c31b0
newrav1k <kritsky.academi@gmail.com>
2025-04-09 01:40:55 +0300

    Added parameters to application.yaml in manager-app

--- manager-app/src/main/java/org/example/managerapp/configuration/ClientConfiguration.java
+++ manager-app/src/main/java/org/example/managerapp/configuration/ClientConfiguration.java
@@ -4,15 +4,20 @@ import org.example.managerapp.client.RestClientTaskClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientConfiguration {
 
     @Bean
-    public RestClientTaskClient restClientTaskClient(@Value("${newrav1k.services.catalogue.url:http://localhost:8081}") String baseUrl) {
+    public RestClientTaskClient restClientTaskClient(@Value("${newrav1k.services.catalogue.url:http://localhost:8081}") String baseUrl,
+                                                     @Value("${newrav1k.services.catalogue.username:}") String catalogueUsername,
+                                                     @Value("${newrav1k.services.catalogue.password:}") String cataloguePassword) {
         return new RestClientTaskClient(RestClient.builder()
                 .baseUrl(baseUrl)
+                .requestInterceptor(
+                        new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
                 .build());
     }
 
