04d6d73dac9c45deed2760c12cad94beecf44642
newrav1k <kritsky.academi@gmail.com>
2025-04-09 01:40:37 +0300

    Added parameters to application.yaml in catalogue-service

--- catalogue-service/src/main/java/org/example/catalogueservice/configuration/SecurityConfiguration.java
+++ catalogue-service/src/main/java/org/example/catalogueservice/configuration/SecurityConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +13,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                        authorizationManagerRequestMatcherRegistry.anyRequest().hasAuthority("ROLE_SERVICE"))
-                .httpBasic(Customizer.withDefaults());
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/task-api/**").hasRole("SERVICE"))
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
