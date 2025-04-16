6ab76f597a6d3cc8ccdf6c598395db3d2e0c3ca7
newrav1k <kritsky.academi@gmail.com>
2025-04-16 20:26:15 +0300

    Added addViewControllers to WebConfiguration in manager-app

---
+++ manager-app/src/main/java/org/example/managerapp/configuration/WebConfiguration.java
@@ -0,0 +1,16 @@
+package org.example.managerapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("auth");
+        registry.addViewController("/register").setViewName("register");
+    }
+
+}
