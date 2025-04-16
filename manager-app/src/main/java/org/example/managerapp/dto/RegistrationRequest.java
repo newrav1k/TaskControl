198787d51765c7e8605ee769f16d78f316fff251
newrav1k <kritsky.academi@gmail.com>
2025-04-16 20:26:37 +0300

    Added RegistrationRequest

---
+++ manager-app/src/main/java/org/example/managerapp/dto/RegistrationRequest.java
@@ -0,0 +1,18 @@
+package org.example.managerapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationRequest {
+
+    private String username;
+
+    private String password;
+
+    private String confirmPassword;
+
+}
