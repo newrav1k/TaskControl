e7cff64c248fc3cfca3f77d5993d768a4e0d6af4
newrav1k <kritsky.academi@gmail.com>
2025-04-18 18:07:15 +0300

    Added valid RegistrationRequest

--- manager-app/src/main/java/org/example/managerapp/dto/RegistrationRequest.java
+++ manager-app/src/main/java/org/example/managerapp/dto/RegistrationRequest.java
@@ -1,18 +1,30 @@
 package org.example.managerapp.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegistrationRequest {
 
+    @NotEmpty(message = "{user.error.username.null}")
     private String username;
 
+    @NotEmpty(message = "{user.error.email.null}")
+    @Email(message = "{user.error.email.invalid}")
+    private String email;
+
+    @ToString.Exclude
+    @NotEmpty(message = "{user.error.password.null}")
     private String password;
 
+    @ToString.Exclude
+    @NotEmpty(message = "{user.error.password.null}")
     private String confirmPassword;
 
 }
