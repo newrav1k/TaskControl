5cc5f1548f348730b5b536fc86affa7c694a854e
newrav1k <kritsky.academi@gmail.com>
2025-04-18 18:06:36 +0300

    Added beans to SecurityConfiguration in manager-app

--- manager-app/src/main/java/org/example/managerapp/configuration/SecurityConfiguration.java
+++ manager-app/src/main/java/org/example/managerapp/configuration/SecurityConfiguration.java
@@ -2,12 +2,23 @@ package org.example.managerapp.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,6 +35,11 @@ public class SecurityConfiguration {
                                 .defaultSuccessUrl("/tasks/list", true)
                                 .permitAll());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
