49bbee62fa8c5c14fbd6d1a6ad38683adb4eada6
newrav1k <kritsky.academi@gmail.com>
2025-04-19 09:55:12 +0300

    Update TaskResponse

--- catalogue-service/src/main/java/org/example/catalogueservice/payload/response/TaskResponse.java
+++ catalogue-service/src/main/java/org/example/catalogueservice/payload/response/TaskResponse.java
@@ -4,14 +4,14 @@ import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.example.catalogueservice.entity.TaskStatus;
 
 import java.time.LocalDateTime;
 
 public record TaskResponse(
+        Integer id,
         @NotBlank(message = "{payload.title.error.blank}") @Size(max = 50, message = "{payload.title.error.size}") String title,
         @NotBlank(message = "{payload.description.error.blank}") @Size(max = 250, message = "{payload.description.error.size}") String description,
-        @NotNull(message = "{payload.status.error.null}") TaskStatus status,
+        @NotNull(message = "{payload.status.error.null}") String status,
         @NotNull(message = "{payload.deadline.error.null}") @FutureOrPresent(message = "{payload.deadline.error.future}") LocalDateTime deadline
 ) {
 
